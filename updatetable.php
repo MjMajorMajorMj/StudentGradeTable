00b1ff6262ccb0451e824635a4fc3ba8625b4ab2
MjMajorMajorMj <techk8@gmail.com>
2018-09-04 13:11:49 -0700

    Fixed updatetable according to server

--- updatetable.php
+++ updatetable.php
@@ -4,7 +4,7 @@ require('mysql_connect.php');
 
 $filename = 'studentdataupdate.sql';
 $tempvar = '';
-$lines = file($filename);
+$lines = file($filename, FILE_USE_INCLUDE_PATH);
 foreach ($lines as $line) {
     if (substr($line, 0, 2) == '--' || $line == '') {
         continue;
@@ -15,6 +15,3 @@ foreach ($lines as $line) {
         $tempvar = '';
     };
 };
-
-
-?>
